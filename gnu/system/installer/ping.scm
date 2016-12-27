dongcarl/guix

commit cd6fd079e61c07135902b673991164ba705c037c

installer: Use %default-subsitute-urls instead of our own variable.
* gnu/system/installer/network.scm: Use %default-substitute-servers.
* gnu/system/installer/ping.scm (subsitute-servers): Delete variable,
 (ping-page-key-handler) Use %default-substitute-servers.

gnu/system/installer/ping.scm
@@ -21,15 +21,13 @@
   #:use-module (gnu system installer page)
   #:use-module (gnu system installer misc)
   #:use-module (gnu system installer utils)
+  #:use-module (guix store)
   #:use-module (gurses buttons)
   #:use-module (ncurses curses)
 
-  #:export (substitution-servers)
   #:export (ping-page-refresh)
   #:export (ping-page-key-handler))
 
-
-(define substitution-servers '("mirror.hydra.gnu.org"))
 
 (define my-buttons `((test ,(N_ "_Test") #t)
 		     (continue  ,(N_ "_Continue") #t)
@@ -67,9 +65,9 @@
      
      ((buttons-key-matches-symbol? nav ch 'test)
 
-      (let* ((windowp (make-window-port test-window)))
+      (let* ();;(windowp (make-window-port test-window)))
 	(if (zero?
-	     (window-pipe test-window  "ping" "ping" "-c" "3" (car substitution-servers)))
+	     (window-pipe test-window  "ping" "ping" "-c" "3" (car %default-substitute-urls)))
 	    (addstr test-window
 		    (gettext "Test successful.  Network is working."))
 	    (addstr test-window

gnu/system/installer/network.scm
@@ -26,6 +26,7 @@
   #:use-module (gurses menu)
   #:use-module (gurses buttons)
   #:use-module (ncurses curses)
+  #:use-module (guix store)
 
   #:export (make-network-page))
 
@@ -158,7 +159,7 @@
 
     (addstr*   text-window  (format #f
 	      (gettext
-	       "To install GuixSD a connection to ~a must be available.  The following network devices exist on the system.  Select one to configure or \"Continue\" to proceeed.") (car substitution-servers)))
+	       "To install GuixSD a connection to one of ~s must be available.  The following network devices exist on the system.  Select one to configure or \"Continue\" to proceeed.") %default-substitute-urls))
     
     (page-set-wwin! p pr)
     (page-set-datum! p 'menu menu)

commit b574581df42fe46619908f991ccba15fd0ae52d3

installer: Fix incorrect host in ping task.
* gnu/system/installer/ping.scm (ping-page-key-handler): Use host part of uri
only.  Commit cd6fd079e61c07135902b673991164ba705c037c inadvertently introduced
this bug.

gnu/system/installer/ping.scm
@@ -24,7 +24,7 @@
   #:use-module (guix store)
   #:use-module (gurses buttons)
   #:use-module (ncurses curses)
-
+  #:use-module (web uri)
   #:export (ping-page-refresh)
   #:export (ping-page-key-handler))
 
@@ -45,10 +45,10 @@
       (cond
        ((eqv? (buttons-selected nav) (1- (buttons-n-buttons nav)))
 	(buttons-unselect-all nav))
-       
+
        (else
 	(buttons-select-next nav))))
-     
+
      ((eq? ch KEY_LEFT)
       (buttons-select-prev nav))
 
@@ -62,17 +62,20 @@
       (delwin (page-datum page 'test-window))
       (set! page-stack (cdr page-stack))
       )
-     
-     ((buttons-key-matches-symbol? nav ch 'test)
 
-      (let* ();;(windowp (make-window-port test-window)))
+     ((buttons-key-matches-symbol? nav ch 'test)
+      (let* ()
 	(if (zero?
-	     (window-pipe test-window  "ping" "ping" "-c" "3" (car %default-substitute-urls)))
+	     (window-pipe test-window  "ping" "ping" "-c" "3"
+                          (uri-host
+                           (string->uri
+                            (car %default-substitute-urls)))))
+
 	    (addstr test-window
 		    (gettext "Test successful.  Network is working."))
 	    (addstr test-window
 		    (gettext "Test failed. No servers reached.")))
-     
+
 	(refresh test-window)))) #f))
 
 (define (ping-page-refresh page)
@@ -113,6 +116,3 @@
     (buttons-post buttons button-window)
     (refresh text-window)
     (refresh button-window)))
-
-			      
-